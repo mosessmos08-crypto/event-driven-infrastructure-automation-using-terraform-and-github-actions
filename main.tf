cb37a0c047606ce8bec39058b0626aaa991e52cc
Moses <mosessmos08@gmail.com>
2026-05-09 17:19:59 +0530

    third commit

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "app" {
-  ami           = "ami-0f5ee92e2d63afc18"   
-  instance_type = "t2.micro"
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"   
+  instance_type = "t3.micro"
 
   tags = {
     Name = "pr-env-${var.pr_id}"
